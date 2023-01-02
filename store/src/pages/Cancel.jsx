ee857c28ac753a98a9a0d91693c65f6dd54cbd9c
Lannie <lannie.shao@gmail.com>
2023-01-02 15:39:07 +1100

    copy code from store to depoly

--- store/src/pages/Cancel.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const cancel = () => {
-  return (
-    <div>You cancel your payment.</div>
-  )
-}
-
-export default cancel
--- store/src/pages/Success.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Success = () => {
-  return (
-    <div>Thanks for your purchase.</div>
-  )
-}
-
-export default Success
